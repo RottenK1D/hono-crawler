1d34dcaa396f47e5c19157176d2c829f57ff82a9
Rinards Sondors <rinards.sondors@gmail.com>
2024-01-12 18:32:08 +0000

    refactor normalizeUrl()

--- src/crawl.ts
+++ src/crawl.ts
@@ -1,6 +1,20 @@
-function normalizeUrl(url: string) {
-	const newObj = new URL(url);
-	return url.slice(newObj.protocol.length + 2);
+function normalizeUrl(url: string): string | Error {
+	if (!url) return new Error("URL is empty");
+
+	try {
+		const newObj = new URL(url);
+		const normalizedUrl = url.slice(newObj.protocol.length + 2);
+
+		if (normalizedUrl.endsWith("/")) {
+			return normalizedUrl.slice(0, -1);
+		}
+
+		return normalizedUrl;
+	} catch (error) {
+		console.error(error);
+		return new Error("URL is invalid");
+	}
 }
 
-normalizeUrl("https://www.google.com");
+const a = normalizeUrl("https://www.google.com");
+console.log(a);
